3821bbaf70d37dad0737efa2c24af6b3521a3a74
Shardul Sisodiya <98705625+shardul523@users.noreply.github.com>
2024-02-16 02:49:13 +0530

    Added Tour Model and implemented tour controllers

--- controllers/toursControllers.js
+++ controllers/toursControllers.js
@@ -1,13 +1,53 @@
-exports.getAllTours = (req, res) => {
-  res.send("Sent all tours");
+const Tour = require("../models/tourModel");
+
+exports.getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find();
+
+    res.status(200).json({
+      status: "success",
+      data: { tours },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err,
+    });
+  }
 };
 
-exports.createNewTour = (req, res) => {
-  res.send("New Tour created");
+exports.createNewTour = async (req, res) => {
+  try {
+    const newTour = await Tour.create(req.body);
+
+    res.status(201).json({
+      status: "success",
+      data: {
+        tour: newTour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err,
+    });
+  }
 };
 
-exports.getTourById = (req, res) => {
-  res.send("New Tour created");
+exports.getTourById = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
+
+    res.status(200).json({
+      status: "success",
+      data: { tour },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err,
+    });
+  }
 };
 
 exports.updateTourById = (req, res) => {
--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 require("dotenv").config({ path: ".env.local" });
+const mongoose = require("mongoose");
 const app = require("./app");
+
 const PORT = +process.env.PORT || 3000;
+const DB_URI = process.env.DATABASE_URI.replace(
+  "<password>",
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose.connect(DB_URI).then(() => console.log("DB connected successfully"));
 
 app.listen(PORT, () => console.log("Listening on PORT", PORT));
