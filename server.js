93b2e1f37116905aa3e804543a8cd28e9a0424eb
Shardul Sisodiya <98705625+shardul523@users.noreply.github.com>
2024-02-20 17:55:01 +0530

    Handled uncaught exceptions and unhandled promise rejections.

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 require("dotenv").config({ path: ".env.local" });
 const mongoose = require("mongoose");
+
+// HANDLING UNCAUGHT EXCEPTIONS (SYNC CODE) THROUGHOUT THE APP
+process.on("uncaughtException", (err) => {
+  console.error("UNCAUGHT EXCEPTION FOUND💥 SHUTTING DOWN...");
+  console.error(err.name, err.message);
+
+  process.exit(1);
+});
+
 const app = require("./app");
 
 const PORT = +process.env.PORT || 3000;
@@ -10,4 +19,12 @@ const DB_URI = process.env.DATABASE_URI.replace(
 
 mongoose.connect(DB_URI).then(() => console.log("DB connected successfully"));
 
-app.listen(PORT, () => console.log("Listening on PORT", PORT));
+const server = app.listen(PORT, () => console.log("Listening on PORT", PORT));
+
+// HANDLING UNHANDLED PROMISE REJECTIONS (ASYNC CODE) THROUGHOUT THE APP
+process.on("unhandledRejection", (err) => {
+  console.error("Unhandled Promise Rejection".toUpperCase());
+  console.error(err.name, err.message);
+
+  server.close(() => process.exit(1));
+});
