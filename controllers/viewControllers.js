65b4d62de60eecbd1f2e467c704905c4659fa983
Shardul Sisodiya <98705625+shardul523@users.noreply.github.com>
2024-02-26 15:25:48 +0530

    Started with tours overview page

--- controllers/viewControllers.js
+++ controllers/viewControllers.js
@@ -1,3 +1,9 @@
-exports.getToursOverview = (req, res) => res.status(200).render("overview");
+const { Tour } = require("../models");
+const { catchAsync } = require("../utils");
 
+exports.getToursOverview = catchAsync(async (req, res) => {
+  const tours = await Tour.find();
+
+  res.status(200).render("overview", { tours });
+});
 exports.getTour = (req, res) => res.status(200).render("tour");
